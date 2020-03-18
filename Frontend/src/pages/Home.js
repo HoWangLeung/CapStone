ee61d8c79f82ec04963e6020deea7f6374ba8f65
Derek Leung <hkz88i00123@gmail.com>
2020-03-18 10:52:12 +0800

    landing

--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -1,27 +1,39 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import "./Home.css";
-// import { makeStyles } from "@material-ui/core/styles";
-// const useStyles = makeStyles({
-//   root: {
-//     width: "100%",
-//     objectFit: "cover"
-//   },
-//   gradient: {
-//     height: "100vh",
-//     width: "100%",
-//     objectFit: "cover",
-//     backgroundSize: "cover",
-//     background:
-//       "linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url('https://images.unsplash.com/photo-1459755486867-b55449bb39ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60') "
-//   }
-// });
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary
+  }
+}));
 
 export default function Home(props) {
   // const classes = useStyles(props);
+  const classes = useStyles();
   return (
-    <Grid container className="resize">
-      <div className="gradient"> </div>
-    </Grid>
+    <div className={classes.root}>
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        justify="center"
+        className="gradient"
+      >
+        <Grid align="center" item xs={12}>
+          <h1 className="Homeh1">Welcome to my coffee shop</h1>
+          <Button variant="contained" color="secondary">
+            Explore
+          </Button>
+        </Grid>
+      </Grid>
+    </div>
   );
 }
